2d9e3819d2f3df0c161b8d1dd94a89eeef89c1e8
Galen Beck Gatzke <gbgatzke@gmail.com>
2022-12-19 10:11:13 -0700

    commit tree 45b6196541560363e396276a6b684d1515aab547

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+//findMatching = (drivers, string) => {
+//    return drivers.filter(function (name) = {
+//        return 
+//    })
+//}
+
+function findMatching(drivers, name) {
+      return drivers.filter(element => element.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(element => element.charAt(0) === name.charAt(0));
+}
+
+function matchName(drivers, search){
+    return drivers.filter(element => element.name === search);
+}
+
